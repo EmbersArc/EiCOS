1f6be2dcac5e1a1c3c87539b6d69f223625dabec
Sven Niederberger <s-niederberger@outlook.com>
2020-02-18 23:39:19 +0100

    add Setup

--- include/ecos.hpp
+++ include/ecos.hpp
@@ -11,26 +11,31 @@ class ECOSEigen
     // The first l elements of s are >=0, ncones is the number of second-order cones present in K.
     // ncones:  Number of second order cones in K.
     // q:       Vector of dimesions of each cone constraint in K.
+
     // A(p,n):  Equality constraint matrix.
     // b(p):    Equality constraint vector.
     // G(m,n):  Generalized inequality matrix.
     // h(m):    Generalized inequality vector.
     // c(n):    Variable weights.
 
-    ECOSEigen(Eigen::SparseMatrix<double> &G,
-              Eigen::SparseMatrix<double> &A,
-              Eigen::SparseMatrix<double> &c,
-              Eigen::SparseMatrix<double> &h,
-              Eigen::SparseMatrix<double> &b,
-              std::vector<size_t> &soc_dims);
+    ECOSEigen(const Eigen::SparseMatrix<double> &G,
+              const Eigen::SparseMatrix<double> &A,
+              const Eigen::SparseMatrix<double> &c,
+              const Eigen::SparseMatrix<double> &h,
+              const Eigen::SparseMatrix<double> &b,
+              const std::vector<size_t> &soc_dims);
 
 private:
-    size_t num_eq; // number of equality constraints (p)
-    size_t num_po; // number of positive constraints (l)
-    size_t num_so; // number of second order cone constraints (ncones)
+    size_t num_var;
+    size_t num_eq;   // number of equality constraints (p)
+    size_t num_ineq; // number of inequality constraints (m)
+    size_t num_pc;   // number of positive constraints (l)
+    size_t num_sc;   // number of second order cone constraints (ncones)
+
+    const double delta_reg = 7e-8; // Static Regularization
+
     Eigen::SparseMatrix<double> K;
-    void SetupKKT(Eigen::SparseMatrix<double> &G,
-                  Eigen::SparseMatrix<double> &A,
-                  Eigen::SparseMatrix<double> &c,
-                  std::vector<size_t> &soc_dims);
+    void SetupKKT(const Eigen::SparseMatrix<double> &G,
+                  const Eigen::SparseMatrix<double> &A,
+                  const std::vector<size_t> &soc_dims);
 };
--- src/ecos.cpp
+++ src/ecos.cpp
@@ -1,83 +1,169 @@
 #include "ecos.hpp"
 
-ECOSEigen::ECOSEigen(Eigen::SparseMatrix<double> &G,
-                     Eigen::SparseMatrix<double> &A,
-                     Eigen::SparseMatrix<double> &c,
-                     Eigen::SparseMatrix<double> &h,
-                     Eigen::SparseMatrix<double> &b,
-                     std::vector<size_t> &soc_dims)
+#include <Eigen/SparseCholesky>
+
+ECOSEigen::ECOSEigen(const Eigen::SparseMatrix<double> &G,
+                     const Eigen::SparseMatrix<double> &A,
+                     const Eigen::SparseMatrix<double> &c,
+                     const Eigen::SparseMatrix<double> &h,
+                     const Eigen::SparseMatrix<double> &b,
+                     const std::vector<size_t> &soc_dims)
 {
+    num_var = A.cols();
     num_eq = A.rows();
-    num_so = soc_dims.size();
-    num_po = h.rows() - num_so;
+    num_sc = soc_dims.size();
+    num_ineq = G.rows(); // = num_pc + num_sc
+    num_pc = num_ineq - num_sc;
+
+    SetupKKT(G, A, soc_dims);
 }
 
-void ECOSEigen::SetupKKT(Eigen::SparseMatrix<double> &G,
-                         Eigen::SparseMatrix<double> &A,
-                         Eigen::SparseMatrix<double> &c,
-                         std::vector<size_t> &soc_dims)
+void ECOSEigen::SetupKKT(const Eigen::SparseMatrix<double> &G,
+                         const Eigen::SparseMatrix<double> &A,
+                         const std::vector<size_t> &soc_dims)
 {
     /**
      *      [ 0  A' G' ]
      *  K = [ A  0  0  ]
      *      [ G  0  -V ]
      * 
-     *   V = blkdiag(I, blkdiag(I,1,-1), ...,  blkdiag(I,1,-1));
-     *                    ^ #number of second-order cones ^
-     *               ^ size is number of positive contraints
+     *   V = blkdiag(I, blkdiag(I, 1, -1), ...,  blkdiag(I, 1, -1));
+     *                    ^   number of second-order cones   ^
+     *               ^ dimension of positive contraints
+     * 
+     *  Only the upper triangular part is constructed here.
      **/
 
-    const size_t K_dim = A.rows() + A.cols() + G.rows() + 2 * soc_dims.size();
+    const size_t K_dim = num_var + num_eq + num_ineq + 2 * num_sc;
+    //                                                   ^ expanded scalings
     K.resize(K_dim, K_dim);
 
     Eigen::SparseMatrix<double> At = A.transpose();
     Eigen::SparseMatrix<double> Gt = G.transpose();
 
-    // add A
-    for (int row = 0; row < A.rows(); row++)
+    size_t K_nonzeros = At.nonZeros() + Gt.nonZeros();
+    // Static Regularization
+    K_nonzeros += num_var + num_eq;
+    // Positive part of scaling block V
+    K_nonzeros += num_pc;
+    for (size_t cone_dim : soc_dims)
     {
-        for (int col = 0; col < A.cols(); col++)
+        // SC part of scaling block V
+        K_nonzeros += 3 * cone_dim + 1;
+    }
+    K.reserve(K_nonzeros);
+
+    std::vector<Eigen::Triplet<double>> K_triplets;
+    K_triplets.reserve(K_nonzeros);
+
+    // Static Regularization of blocks (1,1) and (2,2)
+    for (size_t k = 0; k < num_var; k++)
+    {
+        K_triplets.emplace_back(k, k, delta_reg);
+    }
+    for (size_t k = 0; k < num_eq; k++)
+    {
+        K_triplets.emplace_back(num_var + k, num_var + k, -delta_reg);
+    }
+
+    // A'
+    for (int k = 0; k < At.outerSize(); k++)
+    {
+        for (Eigen::SparseMatrix<double>::InnerIterator it(At, k); it; ++it)
         {
-            K.insert(At.cols() + row, col) = A.coeff(row, col);
+            K_triplets.emplace_back(it.row(), it.col() + A.cols(), it.value());
         }
     }
 
-    // add A'
-    for (int row = 0; row < At.rows(); row++)
+    // G'
     {
-        for (int col = 0; col < At.cols(); col++)
+        Eigen::SparseMatrix<double> Gt_block;
+        size_t col_K = A.cols() + At.cols();
+
+        Gt_block = Gt.leftCols(num_pc);
+        for (int k = 0; k < Gt_block.outerSize(); k++)
         {
-            K.insert(row, A.cols() + col) = At.coeff(row, col);
+            for (Eigen::SparseMatrix<double>::InnerIterator it(At, k); it; ++it)
+            {
+                K_triplets.emplace_back(it.row(), col_K + it.col(), it.value());
+            }
+        }
+        col_K += num_pc;
+
+        size_t col_Gt = col_K;
+        for (size_t cone_dim : soc_dims)
+        {
+            Gt_block = Gt.middleCols(col_Gt, cone_dim);
+            for (int k = 0; k < Gt_block.outerSize(); k++)
+            {
+                for (Eigen::SparseMatrix<double>::InnerIterator it(Gt_block, k); it; ++it)
+                {
+                    K_triplets.emplace_back(it.row(), col_K + it.col(), it.value());
+                }
+            }
+            col_K += cone_dim + 2;
+            col_Gt += cone_dim;
         }
     }
-    
-    // add G
-    for (int row = 0; row < G.rows(); row++)
+
+    // -V
     {
-        for (int col = 0; col < G.cols(); col++)
+        size_t row_col = A.cols() + At.cols();
+
+        // First identity block of -V
+        for (size_t k = 0; k < num_pc; k++)
         {
-            if (G.)
-            K.insert(At.rows() + A.rows() + row, col) = G.coeff(row, col);
+            K_triplets.emplace_back(row_col, row_col, -1.);
+            row_col++;
+        }
+
+        // SOC parts of -V
+        /**
+         * The scaling matrix has the following structure:
+         *
+         *    [ 1                * ]
+         *    [   1           *  * ]
+         *    [     .         *  * ]      
+         *    [       .       *  * ]       [ D   v  u  ]      D: Identity of size conesize       
+         *  - [         .     *  * ]  =  - [ u'  1  0  ]      v: Vector of size conesize - 1      
+         *    [           1   *  * ]       [ v'  0' -1 ]      u: Vector of size conesize    
+         *    [             1 *  * ]
+         *    [   * * * * * * 1    ]
+         *    [ * * * * * * *   -1 ]
+         *
+         *  Only the upper triangular part is constructed here.
+         **/
+        for (size_t cone_dim : soc_dims)
+        {
+            for (size_t k = 0; k < cone_dim; k++)
+            {
+                K_triplets.emplace_back(row_col, row_col, -1.);
+                row_col++;
+            }
+
+            row_col++;
+            K_triplets.emplace_back(row_col, row_col, -1.);
+
+            // -v
+            for (size_t k = 1; k < cone_dim; k++)
+            {
+                K_triplets.emplace_back(row_col - cone_dim + k, row_col, -1.);
+            }
+
+            row_col++;
+            K_triplets.emplace_back(row_col, row_col, 1.);
+
+            // -u
+            for (size_t k = 0; k < cone_dim; k++)
+            {
+                K_triplets.emplace_back(row_col - cone_dim - 1 + k, row_col, -1.);
+            }
         }
     }
 
-    // add G'
-    for (int row = 0; row < Gt.rows(); row++)
-    {
-        for (int col = 0; col < Gt.cols(); col++)
-        {
-            K.insert(row, A.cols() + At.cols() + col) = Gt.coeff(row, col);
-        }
-    }
+    K.setFromTriplets(K_triplets.begin(), K_triplets.end());
+    assert(size_t(K.nonZeros()) == K_nonzeros);
 
-
-
-    // add linear parts to V
-
-    // add SOC parts to V
-    for (size_t c = 0; c < soc_dims.size(); c++)
-    {
-    }
-
-    K.makeCompressed();
+    Eigen::SimplicialLDLT<Eigen::SparseMatrix<double>, Eigen::Upper> ldlt;
+    ldlt.compute(K);
 }
