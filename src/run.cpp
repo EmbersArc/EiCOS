ff658d3f24423feacc975a52dc9caa32caad1568
Sven Niederberger <s-niederberger@outlook.com>
2020-03-01 22:17:46 +0100

    solution check

--- src/run.cpp
+++ src/run.cpp
@@ -15,17 +15,29 @@ int main()
     Eigen::Map<Eigen::VectorXd> h(h_, m);
     Eigen::Map<Eigen::VectorXd> c(c_, n);
     Eigen::Map<Eigen::VectorXi> soc_dims(q_, ncones);
+    Eigen::Map<Eigen::VectorXd> x(x_, n);
 
     double t0 = tic();
 
     ECOSEigen solver(G, A, c, h, b, soc_dims);
 
-    fmt::print("Time for setup:    {}ms", toc(t0));
+    fmt::print("Time for setup:    {:.3}ms\n", toc(t0));
     t0 = tic();
 
     solver.solve();
 
-    fmt::print("Time for solve:    {}ms", toc(t0));
+    fmt::print("Time for solve:    {:.3}ms\n", toc(t0));
+
+    bool correct = x.isApprox(solver.x, 1e-6);
+
+    if (correct)
+    {
+        fmt::print("Solution accurate. (error_norm = {:.4e})\n", (x - solver.x).norm());
+    }
+    else
+    {
+        fmt::print("Solution inaccurate. (error_norm = {:.4e})\n", (x - solver.x).norm());
+    }
 
     fmt::print("\n\n");
     // fmt::print("x=\n{}\n", solver.x);
