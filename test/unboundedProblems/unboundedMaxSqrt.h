f314ab25dfb18b20feef23cc71e5c7834b273b55
Sven Niederberger <s-niederberger@outlook.com>
2020-03-07 19:11:09 +0100

    revert change

--- test/unboundedProblems/unboundedMaxSqrt.h
+++ test/unboundedProblems/unboundedMaxSqrt.h
@@ -30,6 +30,6 @@ else exitflag = ECOS_FATAL;
 /* clean up memory */
 ECOS_cleanup(mywork, 0);
  
-mu_assert("unboundedMaxSqrt: ECOS failed to produce outputflag DUAL INFEASIBLE", exitflag == ECOS_NUMERICS );
+mu_assert("unboundedMaxSqrt: ECOS failed to produce outputflag DUAL INFEASIBLE", exitflag == ECOS_DINF);
 return 0;
 }
